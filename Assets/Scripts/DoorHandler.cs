f6ce6e64567c9933edf8c21c65b434b9b9ba1bb0
Mitchell <mmoscovics@yahoo.com>
2021-05-10 18:54:22 -0400

    [Update] door interact

---
+++ Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHandler : MonoBehaviour
+{
+    private Animator door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        door = gameObject.GetComponent<Animator>();
+    }
+
+    public void Interact()
+    {
+        if (door.GetBool("character_nearby") == false)
+            door.SetBool("character_nearby", true);
+        else
+            door.SetBool("character_nearby", false);
+    }
+}
