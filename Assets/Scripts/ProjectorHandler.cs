551bd1da7b186cb20c3771df9fbb6d9156d67980
Mitchell <mmoscovics@yahoo.com>
2021-05-11 16:10:48 -0400

    [Update] projector work

---
+++ Assets/Scripts/ProjectorHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectorHandler : MonoBehaviour
+{
+    public GameObject particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Interact()
+    {
+        if (particle.activeSelf == true)
+            particle.SetActive(false);
+        else
+            particle.SetActive(true);
+    }
+}
